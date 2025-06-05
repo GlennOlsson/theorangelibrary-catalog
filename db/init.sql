dc58f0e395ff4ab5c42fba43329f80d4ed6c237d
Glenn Olsson <glennholsson@gmail.com>
2025-06-04 23:07:45 -0700

    Add a bunch of init files

---
+++ db/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE koha_theorangelibrary;
+CREATE USER koha_theorangelibrary;
+GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES on koha_theorangelibrary.* TO 'koha_theorangelibrary' WITH GRANT OPTION;
